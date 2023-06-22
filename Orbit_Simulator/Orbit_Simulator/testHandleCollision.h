797325266b7fc07e45d8a6e6c72935612bbe25fd
Marco Varela <marcoavarela99@gmail.com>
2023-06-21 23:46:40 -0600

    Adding the 2 header files

---
+++ Orbit_Simulator/Orbit_Simulator/testHandleCollision.h
@@ -0,0 +1,101 @@
+#pragma once
+/***********************************************************************
+ * Header File:
+ *   testHandleCollision
+ * Author:
+ *   Marco Varela
+ * Summary:
+ *   All the unit tests for handleCollision
+ ************************************************************************/
+
+#include <cassert>
+#include <vector>
+#pragma once
+
+class TestHandleCollision
+{
+public:
+   void run()
+   {
+      test_scenario1_same_position();
+      test_scenario2_samex_samey();
+      test_scenario3_samey_differntx();
+      test_scenario4_different_position();
+   }
+private:
+
+   /*****************************************************
+   * TESTING Scenario 1: Same position
+   *****************************************************/
+   void test_scenario1_same_position()
+   {
+      // Set up
+      StubGps5050 stubGps5050;
+      StubBullet5050 stubBullet5050;
+      Simulator sim;
+      std::vector<OrbitalObject*> orbitalObjects = { &stubGps5050, &stubBullet5050 };
+
+      //Exercise
+      sim.handleCollision()
+
+         //Verify
+         assert(stubGps5050.isDead == true);
+      assert(stubBullet5050.isDead == true);
+   }
+
+   /*****************************************************
+   * TESTING Scenario 2: Same X, different Y
+   *****************************************************/
+   void test_scenario2_samex_samey()
+   {
+      // Set up
+      StubGps5050 stubGps5050;
+      StubBullet50150 stubBullet50150;
+      Simulator sim;
+      std::vector<OrbitalObject*> orbitalObjects = { &stubGps5050, &stubBullet50150 };
+
+      //Exercise
+      sim.handleCollision()
+
+         //Verify
+         assert(stubGps5050.isDead == false);
+      assert(stubBullet50150.isDead == false);
+   }
+   /*****************************************************
+   * TESTING Scenario 3: Same Y, different X
+   *****************************************************/
+   void test_scenario3_samey_differntx()
+   {
+      // Set up
+      StubGps5050 stubGps5050;
+      StubBullet5050 stubBullet15050;
+      Simulator sim;
+      std::vector<OrbitalObject*> orbitalObjects = { &stubGps5050, &stubBullet15050 };
+
+      //Exercise
+      sim.handleCollision()
+
+         //Verify
+         assert(stubGps5050.isDead == false);
+      assert(stubBullet15050.isDead == false);
+   }
+   /*****************************************************
+   * TESTING Scenario 4: Different position
+   *****************************************************/
+   void test_scenario4_different_position()
+   {
+      // Set up
+      StubGps5050 stubGps5050;
+      StubBullet5050 stubBullet150150;
+      Simulator sim;
+      std::vector<OrbitalObject*> orbitalObjects = { &stubGps5050, &stubBullet150150 };
+
+      //Exercise
+      sim.handleCollision()
+
+         //Verify
+         assert(stubGps5050.isDead == false);
+      assert(stubBullet150150.isDead == false);
+   }
+
+};
---
+++ Orbit_Simulator/Orbit_Simulator/testPart.h
@@ -0,0 +1,85 @@
+/***********************************************************************
+* Heaader File:
+*    testPart : Unit test for the Part class
+* Author:
+*    Efrain Gomez Fajardo
+* Summary:
+*    Testing the Part class
+************************************************************************/
+#pragma once
+#include <cassert>
+
+/*******************************
+* TEST Position
+* A friend class for Part which contains the Part unit tests
+********************************/
+class TestPart
+{
+public:
+   void run()
+   {
+      test_constructor_gpscenter();
+      test_constructor_gpsleft();
+      test_constructor_gpsright();
+   }
+private:
+   // Center GPS will stay in the same position as the
+   // full GPS before collision occurred.
+   void test_constructor_gpscenter()
+   {  // setup
+      GPS gps;
+      gps.position.x = 50.0;
+      gps.position.y = 50.0;
+      gps.velocity.dx = 100.0;
+      gps.velocity.dy = 100.0;
+      gps.angle.radians = 3.1415;
+      // exercise
+      Part part(gps);
+      // verify 
+      assert(part.position.x == 50.0);
+      assert(part.position.y == 50.0);
+      assert(part.velocity.dx == 100.0);
+      assert(part.velocity.dy == 100.0);
+      assert(part.angle.radians == 3.1415);
+   }  // teardown
+
+   // Left GPS will be created in a {+10, +10}
+   // position relative to the center
+   void test_constructor_gpsleft()
+   {  // setup
+      GPS gps;
+      gps.position.x = 50.0;
+      gps.position.y = 50.0;
+      gps.velocity.dx = 100.0;
+      gps.velocity.dy = 100.0;
+      gps.angle.radians = 3.1415;
+      // exercise
+      Part part(gps);
+      // verify 
+      assert(part.position.x == 40.0);
+      assert(part.position.y == 40.0);
+      assert(part.velocity.dx == 100.0);
+      assert(part.velocity.dy == 100.0);
+      assert(part.angle.radians == 3.1415);
+   }  // teardown
+
+   // Right GPS will be created in a {-10, -10}
+   // position relative to the center
+   void test_constructor_gpsright()
+   {  // setup
+      GPS gps;
+      gps.position.x = 50.0;
+      gps.position.y = 50.0;
+      gps.velocity.dx = 100.0;
+      gps.velocity.dy = 100.0;
+      gps.angle.radians = 3.1415;
+      // exercise
+      Part part(gps);
+      // verify 
+      assert(part.position.x == 60.0);
+      assert(part.position.y == 60.0);
+      assert(part.velocity.dx == 100.0);
+      assert(part.velocity.dy == 100.0);
+      assert(part.angle.radians == 3.1415);
+   }  // teardown
+}; #pragma once
